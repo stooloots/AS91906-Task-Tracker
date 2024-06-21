8125354acfef5fdded989472e428ef044a324ce2
stooloots <tobywoodmaster666@gmail.com>
2024-06-21 12:40:50 +1200

    Main file version 1
    There is nothing in this file as I am testing the GUI on a different file first

---
+++ GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Task Tracker")
+# Size of GUI window from paint root.geometry("575x370")
+# Size of GUI including border from paint
+root.geometry("586x370")
+root.resizable(0,0)
+root.mainloop()
+
+# Login window
