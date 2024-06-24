80f0a31a352931b8bea7a07532985028ae02afe8
stooloots <tobywoodmaster666@gmail.com>
2024-06-25 10:25:57 +1200

    Login screen MVP
    This file doesn't include any json saving yet. I am trying to implement this in the next version.

--- Login_window.py
+++ Login_window.py
@@ -5,49 +5,90 @@
 
 from tkinter import *
 
-# Login window
+class Window:
+    def __init__ (self):
+        self.root = Tk()
+        # Constants
+        self.COMMON_FONT = ("Calibri", "12")
+        self.LIGHT_BG_COLOUR = "#99B0D0"
+        self.LOGIN_MAIN_COLOUR = "#7092BE"
 
-root = Tk()
-root.title("Task Tracker")
-# Size of GUI window from paint root.geometry("575x370")
-# Size of GUI including border from paint
-root.geometry("586x370")
-root.resizable(0,0)
+        # Login window
+        self.root.title("Task Tracker")
+        # Size of GUI window from paint root.geometry("575x370")
+        # Size of GUI including border from paint
+        self.root.geometry("586x370")
+        self.root.resizable(0,0)
 
-# Window sizing
-root.columnconfigure((0,2), weight= 2)
-root.columnconfigure((1), weight = 1)
-root.rowconfigure((0,2), weight = 2)
-root.rowconfigure((1), weight = 1)
+        # Window sizing
+        self.root.columnconfigure((0,2), weight= 7)
+        self.root.columnconfigure((1), weight = 1)
+        self.root.rowconfigure((0,2), weight = 3)
+        self.root.rowconfigure((1), weight = 1)
 
-# Frame 1 for login system
-login_main_colour = "#7092BE"
-login = Frame(root, bg=login_main_colour, borderwidth=2, relief=SOLID)
-login.grid(column=1, row=1, sticky="NESW")
-login.columnconfigure((0,2), weight= 1)
-login.columnconfigure((1), weight = 4)
-login.rowconfigure((0,2,4,6,8), weight = 1)
-login.rowconfigure((1,3,5,7), weight = 3)
+        # FRAME 1 for login system
+        self.login = Frame(self.root, bg=self.LOGIN_MAIN_COLOUR, borderwidth=2, relief=SOLID)
+        self.login.grid(column=1, row=1, sticky="NESW")
+        self.login.columnconfigure((0,2), weight= 3)
+        self.login.columnconfigure((1), weight = 4)
+        self.login.rowconfigure((0,2,4,6,8), weight = 3)
+        self.login.rowconfigure((1,3,5,7), weight = 3)
 
-# Text lable for login title
-login_text = "User Login"
-login_label = Label(login, text=login_text, font=("Calibri", "12"), bg=login_main_colour)
-login_label.grid(column=1, row=1, sticky="NESW")
+        # Text lable for login title
+        self.login_text = "User Login"
+        self.login_label = Label(self.login, text=self.login_text, font=self.COMMON_FONT, bg=self.LOGIN_MAIN_COLOUR)
+        self.login_label.grid(column=1, row=1, sticky="NESW")
 
-# Frame 2 for Username entry and image
-username = Frame(login, bg="white", borderwidth=2, relief=SOLID)
-username.grid(column=1, row=3)
-username.columnconfigure((0), weight=1)
-username.columnconfigure((1), weight=7)
-username.rowconfigure((0), weight=3)
+        # FRAME 2 for Username entry and image
+        self.username = Frame(self.login, bg="white", borderwidth=2, relief=SOLID)
+        self.username.grid(column=1, row=3, sticky="NESW")
+        self.username.columnconfigure((0), weight=1)
+        self.username.columnconfigure((1), weight=7)
+        self.username.rowconfigure((0), weight=2)
 
-# Frame 2 username image
-username_photoimage = PhotoImage(file = "username_photoimage.png")
-username_image = Label(username, image=username_photoimage)
-username_image.grid(column=0, row=1, sticky="NESW")
+        # Frame 2 username image
+        self.username_photoimage = PhotoImage(file = "username_photoimage.png")
+        self.username_image = Label(self.username, image=self.username_photoimage)
+        self.username_image.grid(column=0, row=0, sticky="NESW")
 
-# Frame 2 username entry
-username_entry_default = "Username"
-username_entry = Entry(username, )
+        # Frame 2 username entry
+        self.username_entry_default = "Username"
+        self.username_entry = Entry(self.username)
+        self.username_entry.insert(0, self.username_entry_default)
+        self.username_entry.grid(column=1, row=0, sticky="NESW")
 
-root.mainloop()
+        # FRAME 3 for Password entry and image
+        self.password = Frame(self.login, bg="white", borderwidth=2, relief=SOLID)
+        self.password.grid(column=1, row=5, sticky="NESW")
+        self.password.columnconfigure((0), weight=1)
+        self.password.columnconfigure((1), weight=7)
+        self.password.rowconfigure((0), weight=2)
+
+        # Frame 3 Password image
+        self.password_photoimage = PhotoImage(file = "password_photoimage.png")
+        self.password_image = Label(self.password, image=self.password_photoimage)
+        self.password_image.grid(column=0, row=0, sticky="NESW")
+
+        # Frame 3 Password entry
+        self.password_entry_default = "Password"
+        self.password_entry = Entry(self.password)
+        self.password_entry.insert(0, self.password_entry_default)
+        self.password_entry.grid(column=1, row=0, sticky="NESW")
+
+        # FRAME 4 for login button
+        self.login_button_frame = Frame(self.login, bg=self.LIGHT_BG_COLOUR, borderwidth=2, relief=SOLID)
+        self.login_button_frame.grid(column=1, row=7, sticky="NESW")
+        self.login_button_frame.columnconfigure((0), weight=1)
+        self.login_button_frame.rowconfigure((0), weight=2)
+
+        # Login button for frame 4
+        self.login_button_text = "Login"
+        self.login_button = Button(self.login_button_frame, text=self.login_button_text, bg=self.LIGHT_BG_COLOUR, font=self.COMMON_FONT, command=lambda: self.username_entry_getter())
+        self.login_button.grid(column=0, row=0, sticky="NESW")
+
+        self.root.mainloop()
+
+    def username_entry_getter(self):
+        users_username = self.username_entry.get()
+        users_password = self.password_entry.get()
+        return print(users_username, users_password)
