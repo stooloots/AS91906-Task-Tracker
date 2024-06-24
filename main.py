96376ca0324f4d9646b1562f7fd784bd11f97d83
stooloots <tobywoodmaster666@gmail.com>
2024-06-25 10:24:33 +1200

    Added login MVP file
    This file doesn't include any functionality besides allowing the user to enter a username and password and see it print in the terminal.

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from tkinter import *
+from Login_window import Window
 
-root = Tk()
-root.title("Task Tracker")
-# Size of GUI window from paint root.geometry("575x370")
-# Size of GUI including border from paint
-root.geometry("586x370")
-root.resizable(0,0)
-root.mainloop()
-
-# Login window
+main = Window()
